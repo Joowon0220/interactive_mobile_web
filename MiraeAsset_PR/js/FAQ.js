aa786c84710a094beefd254eaa336d2d3467c4b3
Joowon <one.joowon@gmail.com>
2021-05-13 14:26:21 +0900

    pdf download

--- MiraeAsset_PR/js/FAQ.js
+++ MiraeAsset_PR/js/FAQ.js
@@ -1,16 +1,5 @@
 $(document).ready(function () {
   $(".anchor").on("click", arrow);
-
-
-  $('.QnA').each(function (i) {
-    console.log("[" + i + "번째]: " + $(this).height());
-    //  var h_QnAs = [$(this).height()];
-    //  var sum = 0;
-    //  for (var h_QnAs;)
-  });
-
-  var hi = $('.FAQs').height();
-  console.log(hi);
 })
 
 
@@ -18,26 +7,16 @@ $(document).ready(function () {
 
 function arrow() {
 
-  // var h_FAQs = $(this).parent().next('.A').height();
-  // console.log(hi);
-
-
   $(this).toggleClass('on');
   if ($(this).parent().next('.A').hasClass('ontxt')) {
-
     $(this).parent().next('.A').css('display', 'none');
     $('.A').removeClass('ontxt');
-
     console.log('a :');
   } else {
     $(this).parent().next('.A').css('display', 'block');
     $(this).parent().next('.A').addClass('ontxt');
     console.log('b');
   }
-
-
-
-  // $(this).parent().next('.A').hasClass('ontxt')
 
 }
 
