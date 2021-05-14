b5824e04e2749838a51b526a9687c5b0eec9f01a
Joowon <one.joowon@gmail.com>
2021-05-14 15:43:10 +0900

    final pc ver

--- MiraeAsset_PR/common/js/navScroll.js
+++ MiraeAsset_PR/common/js/navScroll.js
@@ -9,3 +9,16 @@ $(function () {
     }
   })
 })
+
+//가맹신청 버튼
+function nav_apply() {
+
+  $('.nav_btn').toggleClass('nav_on');
+
+  if ($('.nav_btn').hasClass('nav_on')) {
+    TweenMax.to('.nav_btn', 0.5, { y: 260 })
+  } else {
+    TweenMax.to('.nav_btn', 0.5, { y: 0 })
+  }
+
+}
--- MiraeAsset_PR/js/main.js
+++ MiraeAsset_PR/js/main.js
@@ -44,7 +44,6 @@ function scrollMotion(tl, obj, ofs) {
 $(function () {
   $(window).scroll(function () {
     var navbar = $(this).scrollTop();
-    console.log(navbar);
     if (navbar > 0) {
       $('.nav_img').attr('src', './img/main/nav/main_nav_scroll.png');
     } else {
@@ -52,3 +51,17 @@ $(function () {
     }
   })
 })
+
+
+//가맹신청 버튼
+function nav_apply() {
+
+  $('.nav_btn').toggleClass('nav_on');
+
+  if ($('.nav_btn').hasClass('nav_on')) {
+    TweenMax.to('.nav_btn', 0.5, { y: 260 })
+  } else {
+    TweenMax.to('.nav_btn', 0.5, { y: 0 })
+  }
+
+}
